4eeaa9c703c5ee47b91455dbbcd465bea3587f00
Yilan Wang <wangyilan0304@gmail.com>
2026-06-22 05:50:59 -0400

    rerun evcouplings scoring with more memory allocation

--- EVCouplings/scripts/PG1/score_mutants_Tranception_PG1.sh
+++ EVCouplings/scripts/PG1/score_mutants_Tranception_PG1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -c 1
 #SBATCH -p priority
-#SBATCH --mem=20G
+#SBATCH --mem=60G
 #SBATCH -J tranception_evmutation_scoring
-#SBATCH -t 0-02:00
+#SBATCH -t 0-00:30
 #SBATCH -o tranception_evmutation_scoring.log
 #SBATCH -e tranception_evmutation_scoring.err
 
@@ -13,7 +13,7 @@ conda activate /n/groups/marks/projects/marks_lab_and_oatml/ProteinGym2/model_en
 root=/n/groups/marks/projects/marks_lab_and_oatml/ProteinGym2
 mkdir -p $root/EVCouplings/output/PG1/Tranception_PG1_models_all_EVmutation
 
-for i in {0..216}; do
+for i in 91 184 185 207; do
     echo "DMS_INDEX" $i
     python ./score_mutants_PG1.py \
     --DMS_reference_file_path $root/EVCouplings/input/DMS_substitutions_PG1_trimmed.csv \
